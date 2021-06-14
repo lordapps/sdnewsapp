765db30289de686c09cb7275e815895ccbefa4d5
eltaib <eltaib.hassan@gmail.com>
2021-06-14 08:03:53 +0300

    all Export

--- components/index.js
+++ components/index.js
@@ -1,3 +1,9 @@
 export * from './layout/Layout'
 export * from './layout/MainNavigation'
 
+export * from './meetups/MeetupList'
+export * from './meetups/MeetupItem'
+export * from './meetups/NewMeetupForm'
+
+
+export * from './ui/Card'
--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -1,20 +1,28 @@
-import MeetupItem from './MeetupItem';
+import {MeetupItem} from '../';
 import classes from './MeetupList.module.css';
 
 function MeetupList(props) {
-  return (
-    <ul className={classes.list}>
-      {props.meetups.map((meetup) => (
-        <MeetupItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-        />
-      ))}
-    </ul>
-  );
+    return (<ul className={
+        classes.list
+    }> {
+        props.meetups.map((meetup) => (<MeetupItem key={
+                meetup.id
+            }
+            id={
+                meetup.id
+            }
+            image={
+                meetup.image
+            }
+            title={
+                meetup.title
+            }
+            address={
+                meetup.address
+            }/>))
+    } </ul>);
 }
 
-export default MeetupList;
+export {
+    MeetupList
+};
